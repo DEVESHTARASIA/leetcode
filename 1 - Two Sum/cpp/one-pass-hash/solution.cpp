a307fd087869fa0df22f6ec771403396ef971b33
Devesh Tarasia <DEVESHTARASIA@users.noreply.github.com>
2023-08-15 14:06:05 -0400

    Update solution.cpp

--- 1 - Two Sum/cpp/one-pass-hash/solution.cpp
+++ 1 - Two Sum/cpp/one-pass-hash/solution.cpp
@@ -3,8 +3,8 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         vector<int> ans;
         unordered_map<int,int> seen;
-        for(int i = 0; i < nums.size(); i++) {
-            if(seen.find(target - nums[i]) != seen.end()) {
+        for (int i = 0; i < nums.size(); i++) {
+            if (seen.find(target - nums[i]) != seen.end()) {
                 ans.push_back(i);
                 ans.push_back(seen[target - nums[i]]);
                 return ans;
